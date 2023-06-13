b87ac589d150e408f8941ec781793c143d092a6b
shyamu123 <shyamrabadiya1@gmail.com>
2023-06-13 16:45:26 +0530

    2 commit

--- src/Book.js
+++ src/Book.js
@@ -3,12 +3,16 @@ import React from 'react'
 const Book = (props) => {
     const{UserId,Id,Title,Completed}=props;
   return (
-    <div>
-      <p><b>UserId :</b>{UserId}</p>
-      <p><b>Id :</b>{Id}</p>
-      <p><b>Title :</b>{Title}</p>
-      <p><b>Completed :</b>{Completed}</p>
+    <>
+    <div className='inline-block border-[5px] ml-[10px] max-h-[100px] max-w-[100px] overflow-hidden mt-[5px] bg-green-400 border-yellow-300'>
+      <div className=''>
+        <p><b>UserId :</b>{UserId}</p>
+        <p><b>Id :</b>{Id}</p>
+        <p><b>Title :</b>{Title}</p>
+        <p><b>Completed :</b>{Completed}</p>
+      </div>
     </div>
+    </>
   )
 }
 
