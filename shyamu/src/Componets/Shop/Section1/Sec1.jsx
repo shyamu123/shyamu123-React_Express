5f0472fe05a285378c89efebabd377139de0b218
shyamu123 <shyamrabadiya1@gmail.com>
2023-09-21 17:08:13 +0530

    19 commit

--- shyamu/src/Componets/Shop/Section1/Sec1.jsx
+++ shyamu/src/Componets/Shop/Section1/Sec1.jsx
@@ -1,10 +1,148 @@
 import React from 'react'
-
+import './shopsec1.css';
 const Sec1 = () => {
   return (
-    <div>
-      <h1 className='mt-[100px]'>Shop</h1>
+    <>
+    
+    <div className='containerrr1 m-auto bg-yellow-300 xl:h-[50px] mt-[370px]'>
+      <div className='flex justify-around'>
+        <div>
+          <p>Showing 1–9 of 27 results</p>
+        </div>
+
+        <div className='bg-red-400 h-[40px] w-[250px] flex'>
+        <div>
+        <i class="fa-solid fa-bars"></i>
+
+        </div>
+
+        <div>
+        <i class="fa-solid fa-table-cells-large"></i>
+        </div>
+        
+        <div>
+        </div>
+        </div>
+
+      </div>  
     </div>
+
+
+    <div className='mt-[90px] w-[300px] ms-10'>
+        <details className='group'>
+          <summary className='flex justify-between'>
+            <span className='ipad text-[19px] text-[#232323]'>Ipad Phone & Tablets</span>
+            <span className='group-open:rotate-90'>
+              <svg fill="none" height="24" shapeRendering="geometricPrecision" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="1.3" viewBox="0 0 24 24" width="24" className='-rotate-90'><path d="M6 9l6 6 6-6"></path>
+              </svg>
+            </span>
+          </summary>
+          <ul className='list-disc'>
+            <li className='ms-5 decimal marker:text-[20px]'><a href='#'>Phone</a></li>
+            <li className='ms-5 decimal marker:text-[20px]'><a href='#'>Teblet</a></li>
+            <li className='ms-5 decimal marker:text-[20px]'><a href='#'>IPad</a></li>
+          </ul>
+        </details>
+      </div>
+
+      <div className='mt-1 w-[300px] ms-10'>
+        <details className='group'>
+          <summary className='flex justify-between'>
+            <span className='ipad text-[19px] text-[#232323]'>Planer & Virtual</span>
+            <span className='group-open:rotate-90'>
+              <svg fill="none" height="24" shapeRendering="geometricPrecision" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="1.3" viewBox="0 0 24 24" width="24" className='-rotate-90'><path d="M6 9l6 6 6-6"></path>
+              </svg>
+            </span>
+          </summary>
+          <ul className='list-disc'>
+            <li className='ms-5 decimal marker:text-[20px]'><a href='#'>Planer</a></li>
+            <li className='ms-5 decimal marker:text-[20px]'><a href='#'>Virtual</a></li>
+          </ul>
+        </details>
+      </div>
+
+       <div className='mt-1 w-[300px] ms-10'>
+        <details className='group'>
+          <summary className='flex justify-between'>
+            <span className='ipad text-[19px] text-[#232323]'>Spinning Reel & Kettle</span>
+            <span className='group-open:rotate-90'>
+              <svg fill="none" height="24" shapeRendering="geometricPrecision" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="1.3" viewBox="0 0 24 24" width="24" className='-rotate-90'><path d="M6 9l6 6 6-6"></path>
+              </svg>
+            </span>
+          </summary>
+          <ul className='list-disc'>
+            <li className='ms-5 decimal marker:text-[20px]'><a href='#'>Spinning-Reel</a></li>
+            <li className='ms-5 decimal marker:text-[20px]'><a href='#'>Kettle</a></li>
+          </ul>
+        </details>
+      </div>
+
+       <div className='mt-1 w-[300px] ms-10'>
+        <details className='group'>
+          <summary className='flex justify-between'>
+            <span className='ipad text-[19px] text-[#232323]'>Computers Monitor & Laptop</span>
+            <span className='group-open:rotate-90'>
+              <svg fill="none" height="24" shapeRendering="geometricPrecision" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="1.3" viewBox="0 0 24 24" width="24" className='-rotate-90'><path d="M6 9l6 6 6-6"></path>
+              </svg>
+            </span>
+          </summary>
+          <ul className='list-disc'>
+            <li className='ms-5 decimal marker:text-[20px]'><a href='#'>Computers</a></li>
+            <li className='ms-5 decimal marker:text-[20px]'><a href='#'>Leptop</a></li>
+            <li className='ms-5 decimal marker:text-[20px]'><a href='#'>Monitor</a></li>
+          </ul>
+        </details>
+      </div>
+
+       <div className='mt-1 w-[300px] ms-10'>
+        <details className='group'>
+          <summary className='flex justify-between'>
+            <span className='ipad text-[19px] text-[#232323]'>Exercise Bike & Shaver Clean</span>
+            <span className='group-open:rotate-90'>
+              <svg fill="none" height="24" shapeRendering="geometricPrecision" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="1.3" viewBox="0 0 24 24" width="24" className='-rotate-90'><path d="M6 9l6 6 6-6"></path>
+              </svg>
+            </span>
+          </summary>
+          <ul className='list-disc'>
+            <li className='ms-5 decimal marker:text-[20px]'><a href='#'>Exercise-Bike</a></li>
+            <li className='ms-5 decimal marker:text-[20px]'><a href='#'>Shaver-Clean</a></li>
+          </ul>
+        </details>
+      </div>
+
+       <div className='mt-1 w-[300px] ms-10'>
+        <details className='group'>
+          <summary className='flex justify-between'>
+            <span className='ipad text-[19px] text-[#232323]'>Wireless & Watches</span>
+            <span className='group-open:rotate-90'>
+              <svg fill="none" height="24" shapeRendering="geometricPrecision" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="1.3" viewBox="0 0 24 24" width="24" className='-rotate-90'><path d="M6 9l6 6 6-6"></path>
+              </svg>
+            </span>
+          </summary>
+          <ul className='list-disc'>
+            <li className='ms-5 decimal marker:text-[20px]'><a href='#'>Wireless</a></li>
+            <li className='ms-5 decimal marker:text-[20px]'><a href='#'>Watch</a></li>
+          </ul>
+        </details>
+      </div>
+
+      <div className='mt-1 w-[300px] ms-10'>
+        <details className='group'>
+          <summary className='flex justify-between'>
+            <span className='ipad text-[19px] text-[#232323]'>Camera Bluetooth & Headset</span>
+            <span className='group-open:rotate-90'>
+              <svg fill="none" height="24" shapeRendering="geometricPrecision" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="1.3" viewBox="0 0 24 24" width="24" className='-rotate-90'><path d="M6 9l6 6 6-6"></path>
+              </svg>
+            </span>
+          </summary>
+          <ul className='list-disc'>
+            <li className='ms-5 decimal marker:text-[20px]'><a href='#'>Camera</a></li>
+            <li className='ms-5 decimal marker:text-[20px]'><a href='#'>Bluetooth</a></li>
+            <li className='ms-5 decimal marker:text-[20px]'><a href='#'>Headset</a></li>
+          </ul>
+        </details>
+      </div>
+    </>
   )
 }
 
